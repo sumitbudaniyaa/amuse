f886595213c92d84c18e0864b2926e46b03a7786
sumitbudaniyaa <sumit.k.budaniya@gmail.com>
2025-02-19 16:33:19 +0530

    changes

--- admin-app/src/App.jsx
+++ admin-app/src/App.jsx
@@ -1,35 +1,11 @@
 import "./App.css";
-import Dashboard from "./components/dashboard/dashboard";
-import Login from "./components/login/login";
-import SignUp from "./components/signup/signup";
+import Dash from "./components/dashboard/dashboard";
 import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-      
-           <Login />
-       
-          }
-        ></Route>
-        
-        <Route path="/signup" element={
-         
-          <SignUp />
-        
-        }>
-        </Route>
-        <Route path="/dashboard" element={
-          <Dashboard />
-        }>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+   <Dash />
   );
 }
 
--- admin-app/src/components/dashboard/dashboard.jsx
+++ admin-app/src/components/dashboard/dashboard.jsx
@@ -1,56 +1,20 @@
-import "./dashboard.css";
-import { HiOutlineHome } from "react-icons/hi";
-import { FaRegUser } from "react-icons/fa";
-import { BsQrCodeScan } from "react-icons/bs";
-import { MdOutlineLogout } from "react-icons/md";
-import { IoMdAdd } from "react-icons/io";
-import { useState } from "react";
-import PostEvent from "../posteventpage/postevent";
-import DashMain from "../dashmain/dashmain";
-import { use } from "react";
+import './dashboard.css'
 
-const Dashboard = () => {
+const Dash = () =>{
+    return(
+        <>
+                <div className="dash-header"></div>
+     <div className="dash">
+        <h1>hi</h1>
+     </div>
+     <div className="scan">
 
-  const [currentPage, setcurrentPage] = useState("home");
-  const [submitbtn, setsubmitbtn] = useState(false);
+     </div>
+     </>
 
-  const renderContent = () => {
-    switch (currentPage) {
-      case "home":
-        return <DashMain />;
 
-      case "postevent":
-        return <PostEvent />;
-    }
-  };
 
-  return (
-    <>
-    <div className="dashboard">
-      { renderContent()}
+    )
+}
 
-    </div>
-
-    <div className="dash-header">
-      <ul>
-        <li id="home-icon" onClick={()=>{setcurrentPage('home'); setsubmitbtn(false)}}>
-        <HiOutlineHome />
-        </li>
-        <li id="user-icon">
-        <FaRegUser />
-        </li>
-        <li id="scan-icon">
-        <BsQrCodeScan />
-        </li>
-        <li>
-          <button onClick={()=>{setcurrentPage('postevent'); setsubmitbtn(true); }} className={submitbtn ? 'submitbtn' : ''}>
-            {submitbtn ? 'Submit' : 'Post Event'}
-          </button>
-        </li>
-      </ul>
-    </div>
-    </>
-  );
-};
-
-export default Dashboard;
+export default Dash;
