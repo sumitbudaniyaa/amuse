819b7157b60a9276e154a0354c85237dcd4d111d
sumitbudaniyaa <sumit.k.budaniya@gmail.com>
2025-03-10 13:49:17 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import './App.css'
 import { Routes, Route, useLocation } from 'react-router-dom'
 import Header from './components/header/header'
 import Login from './components/login/login'
-import Home from './components/home/Home'
+import Home from './components/home/home'
 import { useState } from 'react'
 import Account from './components/account/account'
 import BookingPage from './components/bookingpage/bookingpage'
+
 
 function App() {
 
@@ -19,7 +20,7 @@ function App() {
       {!isLoginPage && <Header  isLoggedIn={isLoggedIn}/>}
       <main>
         <Routes>
-          <Route path="/" element={<Home/>} />
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login setisLoggedIn={setisLoggedIn} />}/>
           <Route path="/account" element={<Account setisLoggedIn={setisLoggedIn} />}/>
           <Route path="/booking/:name" element={<BookingPage />} />
